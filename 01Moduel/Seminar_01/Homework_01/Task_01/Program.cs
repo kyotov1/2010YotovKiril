249254a9b7230abcb47f4154378e1406b7f72715
kyotov1 <kyotov1@users.noreply.github.com>
2020-09-08 18:56:44 +0300

    Coments

--- 01Moduel/Seminar_01/Homework_01/Task_01/Program.cs
+++ 01Moduel/Seminar_01/Homework_01/Task_01/Program.cs
@@ -1,16 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+/*
+ * Name: Yotov Kiril
+ * Group: БПИ2010
+ * Excersice: Task from slide 29 from seminar_01
+ */
 
+//Inseption of namespace.
 namespace Task_01
 {
+    /// <summary>
+    /// The main class Program.
+    /// Contains solution of task.
+    /// </summary>
     class Program
     {
+        //Entry point.
         static void Main(string[] args)
         {
-            ///
-        }
-    }
-}
+            
+        }//End Main().
+    }//End Class Program.
+}//End namespace Task_01.
