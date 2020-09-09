3916b8efeb67184d287794b87405e29d1e7a3b66
kyotov1 <kyotov1@users.noreply.github.com>
2020-09-09 19:00:39 +0300

    First Task

--- 01Moduel/Seminar_02/Classwork_02/Task_01/Program.cs
+++ 01Moduel/Seminar_02/Classwork_02/Task_01/Program.cs
@@ -8,9 +8,37 @@ namespace Task_01
 {
     class Program
     {
+        /// <summary>
+        /// Method calculates value of b and returns it.
+        /// </summary>
+        /// <param name="b">Output parameter which saves value.</param>
+        static void Degree(out double b)
+        {
+            b= 0;
+            b = (1 + Math.Sqrt(5)) / 2;
+        }
+
+        /// <summary>
+        /// Method is calulating Un and returning its value.
+        /// </summary>
+        /// <param name="n">Input parameter used for calculating Un</param>
+        /// <returns></returns>
+        static double Un(int n)
+        {
+            
+            Degree(out double b);
+            return (Math.Pow(b,n)-(Math.Pow(-b,-n))/(2*b-1));
+
+        }
+
         static void Main(string[] args)
         {
-            //
+            //variable inicialize
+            int n;
+            do Console.Write("n=");
+            while (!int.TryParse(Console.ReadLine(), out n)|| n<=0) ;
+            Console.WriteLine(Math.Round(Un(n)));
+            
         }
     }
 }
